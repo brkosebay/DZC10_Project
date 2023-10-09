bb830c5a51384b4606a2bb205eed5e610406108f
Berkay Kosebay <b.kosebay@student.tue.nl>
2023-10-09 16:43:56 +0200

    some changes to maze generation

--- DZC10_Project/Assets/Scripts/MazeGenerator.cs
+++ DZC10_Project/Assets/Scripts/MazeGenerator.cs
@@ -10,11 +10,12 @@ public class CaveGeneratorWithFloodFill : MonoBehaviour
     public Tile pathTile;
     public float threshold = 0.4f;
     public int scale = 1;
+    public int x0 = 0, y0 = 0;
 
     private const float PI50000 = Mathf.PI * 50000f;
     private int canvasWidth = 64;
     private int canvasHeight = 64;
-    private int x0 = 0, y0 = 0;
+    
 
     void Start()
     {
@@ -56,7 +57,7 @@ public class CaveGeneratorWithFloodFill : MonoBehaviour
                 Vector3Int position = new Vector3Int(x, y, 0);
                 Tile currentTile = tilemap.GetTile<Tile>(position);
 
-                if (currentTile != pathTile)
+                if (currentTile == wallTile)
                 {
                     bool isAdjacentToPath = false;
                     Vector3Int[] directions = {
@@ -78,27 +79,29 @@ public class CaveGeneratorWithFloodFill : MonoBehaviour
 
                     if (!isAdjacentToPath)
                     {
-                        tilemap.SetTile(position, null);  // Clear the tile.
+                        tilemap.SetTile(position, null);  // Clear the tile if it is not adjacent to any path.
                     }
                 }
             }
         }
     }
 
+
     void GenerateCave()
     {
-        for (int ix = 0, x = x0 - canvasWidth / 2; ix < canvasWidth; ix += scale, x++)
+        for (int ix = 0, x = x0 - canvasWidth / 2; ix < canvasWidth; ix += 3, x++)
         {
-            for (int iy = 0, y = y0 - canvasHeight / 2; iy < canvasHeight; iy += scale, y++)
+            for (int iy = 0, y = y0 - canvasHeight / 2; iy < canvasHeight; iy += 3, y++)
             {
-                Vector3Int position = new Vector3Int(ix, iy, 0);
-                if (IsCaveOpen(x, y, threshold))
+                bool isOpen = IsCaveOpen(x, y, threshold);
+
+                for (int dx = 0; dx < 3; dx++)
                 {
-                    tilemap.SetTile(position, openTile);
-                }
-                else
-                {
-                    tilemap.SetTile(position, wallTile);
+                    for (int dy = 0; dy < 3; dy++)
+                    {
+                        Vector3Int position = new Vector3Int(ix + dx, iy + dy, 0);
+                        tilemap.SetTile(position, isOpen ? openTile : wallTile);
+                    }
                 }
             }
         }
