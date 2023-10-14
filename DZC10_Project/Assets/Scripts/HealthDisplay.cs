5377d6d9889577536a985bbf90d9d13fc36022e1
Berkay Kosebay <b.kosebay@student.tue.nl>
2023-10-14 16:43:37 +0200

    Fixed a bug with script execution order

--- DZC10_Project/Assets/Scripts/HealthDisplay.cs
+++ DZC10_Project/Assets/Scripts/HealthDisplay.cs
@@ -5,9 +5,18 @@ public class HealthDisplay : MonoBehaviour
 {
     public Health playerHealth;
     public Slider healthSlider;
+    public Gradient healthBarGradient;
 
     private void Update()
     {
         healthSlider.value = playerHealth.currentHealth;
+        UpdateHealthBarColor();  // Call the new function to update color
+    }
+
+    void UpdateHealthBarColor() // New function
+    {
+        float healthPercentage = (float)playerHealth.currentHealth / playerHealth.maxHealth;
+        Color currentColor = healthBarGradient.Evaluate(healthPercentage);
+        healthSlider.fillRect.GetComponent<Image>().color = currentColor;
     }
 }
