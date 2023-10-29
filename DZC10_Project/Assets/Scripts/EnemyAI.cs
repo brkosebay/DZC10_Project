7426b11738c8387de5a1d6504cafa6443f9895eb
OctavCrs <91747433+OctavCrs@users.noreply.github.com>
2023-10-29 18:36:11 +0100

    Added and fixed animations for Witch

--- DZC10_Project/Assets/Scripts/EnemyAI.cs
+++ DZC10_Project/Assets/Scripts/EnemyAI.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
-    public float patrolSpeed = 2.0f;
+    public float patrolSpeed = 0.0f;
     public float chaseSpeed = 3.5f;
     public float detectionRadius = 10.0f;
     public float shootingDistance = 7.0f;
@@ -9,6 +9,7 @@ public class EnemyAI : MonoBehaviour {
     public float shootingCooldown = 2.0f;
     public int health = 100;
     public int noOfPoints = 5;
+    public Animator animator;
     public GameObject bulletPrefab;
     public Transform bulletSpawnPoint;
 
@@ -26,13 +27,16 @@ public class EnemyAI : MonoBehaviour {
         if (playerTransform != null) {
             float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
 
-            if (distanceToPlayer <= detectionRadius) {
+            if (distanceToPlayer <= detectionRadius && health > 0) {
                 isChasing = true;
                 ChasePlayer();
-            } else {
-                isChasing = false;
-                Patrol();
             }
+            
+            // else 
+            // {
+            //     isChasing = false;
+            //     Patrol();
+            // }
 
             if (distanceToPlayer <= shootingDistance && shootingTimer <= 0.0f) {
                 Shoot();
@@ -42,6 +46,8 @@ public class EnemyAI : MonoBehaviour {
         if (shootingTimer > 0.0f) {
             shootingTimer -= Time.deltaTime;
         }
+
+        UpdateAnimations();
     }
 
     private void Patrol() {
@@ -56,8 +62,8 @@ public class EnemyAI : MonoBehaviour {
 
     public void TakeDamage(int damage)
     {
+        animator.SetTrigger("IsDamaged");
         health -= damage;
-
         if(health <=0)
         {
             Die();
@@ -66,8 +72,13 @@ public class EnemyAI : MonoBehaviour {
 
     void Die()
     {
-        Destroy(gameObject);
+        enabled = false;
+        animator.SetFloat("Horizontal", (playerTransform.position.x - transform.position.x) * 100);
+        animator.SetTrigger("IsDead");
+        GetComponent<Collider2D>().enabled = false;
+
         GameManager.Instance.IncreaseScore(noOfPoints);
+        Destroy(gameObject, 3f);
     }   
 
     private void Shoot() {
@@ -78,5 +89,11 @@ public class EnemyAI : MonoBehaviour {
         spell.transform.Rotate(0, 0, -Mathf.Atan2(shootingDirection.y, shootingDirection.x) * Mathf.Rad2Deg);
         shootingTimer = shootingCooldown;
 
+    }
+
+    private void UpdateAnimations() {
+        animator.SetFloat("Horizontal", playerTransform.position.x - transform.position.x);
+        animator.SetFloat("Vertical", playerTransform.position.y - transform.position.y);
+        animator.SetFloat("Speed", isChasing ? chaseSpeed : patrolSpeed);
     }
 }
